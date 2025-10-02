27eb87de1b35689624a3df44696341e8b1364899
Oyekunle <oyekunle.alabi@bsu.edu>
2025-10-01 22:21:38 -0400

    Add basics_lab.py: variables, lists, loops, functions

---
+++ basics_lab.py
@@ -0,0 +1,14 @@
+name ="Oye"
+age = 25
+print(f"My name is {name} and I'm {age} years old.")
+
+tools = ["wireshark", "Burp Suite" , "Java" , "Kali Linux"]
+
+for tool in tools:
+    print("tool:" , tool)
+
+def greet_user(name):
+    print(f"Hello! {name} welcome to the Cyber Lab")
+
+greet_user("Oye")
+greet_user("Nestor")
